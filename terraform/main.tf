668b3b11b4f8a3b7f3c1349b99f7ef70e06afacf
Shegolkov-vg <voovik59@gmail.com>
2024-01-16 23:33:32 +0500

    Editing main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
+/*
 terraform {
   required_version = ">= 0.13.0"
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.94.0"
     }
   }
 }
+*/
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
